ac80d5ae71a3efd0954f7a3b04819b7911ae0807
Crihojames <crihojames@gmail.com>
2024-02-17 00:31:15 +0100

    Feat on scroll event

--- src/pages/Netflix.jsx
+++ src/pages/Netflix.jsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
 
 
 const Netflix = () => {
+
+    const [isScrolled, setIsScrolled] = useState(false)
+
+    window.onscroll = ()=>{
+        setIsScrolled(window.pageYoffSet === 0 ? false: true)
+        return ()=>(window.onscroll = null)
+    }
+    console.log(isScrolled);
     return (
-        <div>
-            Netflix
+        <div className="hero">
+            <img src="public\images\avengers age of ultron team together poster.png"  
+            alt="Hero image" />
+            <img src="public\images\avengers age of ultron team together poster.png" 
+            alt="Hero image" />
+            <img src="public\images\avengers age of ultron team together poster.png" 
+            alt="Hero image" />
+            <img src="public\images\avengers age of ultron team together poster.png" 
+            alt="Hero image" />
         </div>
     );
 };
