80773f058ff937eca9852f0ec6d5f74b73cebd44
Crihojames <crihojames@gmail.com>
2024-02-18 01:27:26 +0100

    Feat logOut

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -1,41 +1,52 @@
 import { AiOutlineLogout } from 'react-icons/ai';
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { firebaseAuth } from "../utils/firebase-config";
 
-const TopNav = () => {
-    const navlink = [
-        {name: "Home", link: '/'},
-        {name: "Tv Show", link: '/tv'},
-        {name: "My List", link: '/mylist'},
-        {name: "Movies", link: '/movies'},
-    ]
-    return (
-        <NavContainer>
-            <div className="leftSide">
-                <div className="logo">
-                    <img src="public\images\Netflix_Logo_PMS.png" 
-                    alt="Logo" />
-                </div>
-                <ul className="links">
-                    {
-                        navlink.map(({name, link})=>{
-                            return(
-                                <li key={name}>
-                                    <Link to={link}>{name}</Link>
-                                </li>
-                            )
-                        })
-                    }
-                </ul>
+import { onAuthStateChanged, signOut } from "firebase/auth"
+
+const TopNav = ({ isScrolled }) => {
+  const navlinks = [
+    { name: "Home", link: "/" },
+    { name: "Tv Show", link: "/tv" },
+    { name: "My List", link: "/mylist" },
+    { name: "Movies", link: "/movies" },
+  ];
+
+  const navigate = useNavigate()
+  onAuthStateChanged(firebaseAuth, (currentUser)=>{
+    if(!currentUser)navigate('/login')
+    })
+
+  return (
+    <NavContainer>
+        <nav className={`${isScrolled ? "scrolled" : "notScroll"}`}>
+        <div className="leftSide">
+            <div className="logo">
+            <img
+                src="public\images\Netflix_Logo_PMS.png"
+                alt="Logo"
+            />
             </div>
-            <div className="righSide">
-                <button>
-                    <AiOutlineLogout/>
-                </button>
-            </div>
-        </NavContainer>
-    );
-};
+            <ul className="links">
+            {navlinks.map(({ name, link }) => {
+                return (
+                <li key={name}>
+                    <Link to={link}>{name}</Link>
+                </li>
+                )
+            })}
+            </ul>
+        </div>
+        <div className="righSide">
+            <button onClick={()=>signOut(firebaseAuth)}>
+            <AiOutlineLogout />
+            </button>
+        </div>
+        </nav>
+    </NavContainer>
+  )
+}
 
 const NavContainer = styled.div`
 
